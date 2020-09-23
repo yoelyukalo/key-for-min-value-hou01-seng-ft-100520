54c8ea324ffbe66f5a8b9adfb6aad76ac7195b21
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-23 19:51:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,12 @@
 
 def key_for_min_value(name_hash)
   lowest_key = nil
+<<<<<<< HEAD
   lowest_value = Float::INFINITY
   name_hash.each do |key, value|
+=======
+  hash.each do |key, value|
+>>>>>>> fd80a1628a0db68ca9e174280b5449224effcdfb
     if value < lowest_value
       lowest_value = value
       lowest_key = key
